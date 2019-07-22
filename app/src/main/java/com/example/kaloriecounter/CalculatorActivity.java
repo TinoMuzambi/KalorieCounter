509e3675e0057b820beb763e06efad1381bcfb9b
Tino <tinomuzambi@gmail.com>
2019-07-22 16:40:40 +0200

    Implemented running totals for Calculator.

--- app/src/main/java/com/example/kaloriecounter/CalculatorActivity.java
+++ app/src/main/java/com/example/kaloriecounter/CalculatorActivity.java
@@ -1,11 +1,17 @@
 package com.example.kaloriecounter;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import static android.util.Log.d;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -17,6 +23,14 @@ public class CalculatorActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView foodTotal = findViewById(R.id.foodNumericTotalTextView);
+        TextView exerciseTotal = findViewById(R.id.exerciseNumericTotalTextView);
+        TextView nettTotal = findViewById(R.id.nettNumericTotalTextView);
+
+        foodTotal.setText("0");
+        exerciseTotal.setText("0");
+        nettTotal.setText("0");
     }
 
     // This and the onSaveInstanceState method below handle persisting state when the activity
@@ -26,7 +40,52 @@ public class CalculatorActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
+    public void updateFood(View view) {
+        EditText foodEditText = findViewById(R.id.foodCalorieCountText);
+        TextView foodTotal = findViewById(R.id.foodNumericTotalTextView);
+        TextView exerciseTotal = findViewById(R.id.exerciseNumericTotalTextView);
+        TextView nettTotal = findViewById(R.id.nettNumericTotalTextView);
+
+        try {
+            int foodSum = Integer.valueOf(foodTotal.getText().toString()) + Integer.valueOf(foodEditText.getText().toString());
+            foodTotal.setText(String.valueOf(foodSum));
+        }
+        catch(Exception e) {
+            d("Tino", e.getMessage());
+        }
+        int nettSum = Integer.valueOf(foodTotal.getText().toString()) - Integer.valueOf(exerciseTotal.getText().toString());
+        if (nettSum > 0) {
+            nettTotal.setText(String.valueOf(nettSum));
+        }
+        else {
+            nettTotal.setText("0");
+        }
+    }
+
+    public void updateExercise(View view) {
+        EditText exerciseEditText = findViewById(R.id.exerciseCalorieCountText);
+        TextView foodTotal = findViewById(R.id.foodNumericTotalTextView);
+        TextView exerciseTotal = findViewById(R.id.exerciseNumericTotalTextView);
+        TextView nettTotal = findViewById(R.id.nettNumericTotalTextView);
+
+        try {
+            int exerciseSum = Integer.valueOf(exerciseTotal.getText().toString()) + Integer.valueOf(exerciseEditText.getText().toString());
+            exerciseTotal.setText(String.valueOf(exerciseSum));
+        }
+        catch(Exception e){
+            d("Tino", e.getMessage());
+        }
+        int nettSum = Integer.valueOf(foodTotal.getText().toString()) - Integer.valueOf(exerciseTotal.getText().toString());
+        if (nettSum > 0) {
+            nettTotal.setText(String.valueOf(nettSum));
+        }
+        else {
+            nettTotal.setText("0");
+        }
+    }
+
     public void saveEntry(View view) {
+
     }
 
 }
